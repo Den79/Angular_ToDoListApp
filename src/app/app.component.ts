305d33ba1d36afb0afebf4aea5509e4f122c28aa
Denis Maltev <A01171996@ad.bcit.ca>
2019-11-21 14:48:34 -0800

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'toDoListApp';
+  title = 'To-Do List';
+  //item:string;
+  itemsArray = [];
+  
+  constructor (){ }
+
+  addItem(item:string){
+    this.itemsArray.push(item)
+  }
+
+  deleteItem(){
+    
+  }
 }
